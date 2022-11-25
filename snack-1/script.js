9eaaa7bd676507f21e294eb07cebe73be7a015c4
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-25 15:49:58 +0100

    add ;

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,15 +2,15 @@
 // Chiedi per 6 volte all'utente di inserire un numero,
 // se è dispari inseriscilo nell'array.
 
-let myArray= []
+const myArray= [];
 
 for(let i=0; i<6; i++){
   const myNumber = parseInt(prompt("inserisci un numero"));
 
   if (myNumber % 2 === 0){
-    console.log(myNumber)
-    myArray.push(myNumber)
+    console.log(myNumber);
+    myArray.push(myNumber);
   }
 }
 
-console.log(myArray)
+console.log(myArray);
