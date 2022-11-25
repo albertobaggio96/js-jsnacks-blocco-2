76160ce1904ee13d1832449a72e637d6e19c0cf3
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-25 17:22:21 +0100

    max

--- snack-3 copy/script.js
+++ snack-3 copy/script.js
@@ -1,15 +1,24 @@
 // Calcola la somma dei primi 10 numeri di un array.
 
-const myArray= [1 , 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,];
+const myArray= [1 , 2, 32, 4, 5, 6, 17, 8, 9, 13, 11, 12, 13,];
 
 let sum= 0;
 
+let max= 0;
+
 for(let i = 0 ; i <10; i++){
   sum += myArray[i];
+  if(myArray[i]>max){
+    max = myArray[i];
+  }
+
 }
+
 
 console.log(sum);
 
 let myMedia= sum / 10;
 
 console.log(myMedia);
+
+console.log(max);
