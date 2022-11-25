1a74276313d402da534bb146d75e792a5e49eb54
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-25 16:52:48 +0100

    add ;

--- snack-3 copy/script.js
+++ snack-3 copy/script.js
@@ -5,10 +5,11 @@ const myArray= [1 , 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,];
 let sum= 0;
 
 for(let i = 0 ; i <10; i++){
-  sum += myArray[i]
+  sum += myArray[i];
 }
 
-console.log(sum)
+console.log(sum);
 
-let myMedia= sum / 10
-console.log(myMedia)
+let myMedia= sum / 10;
+
+console.log(myMedia);
