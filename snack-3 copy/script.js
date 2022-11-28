756151c695a545dcce7138825becf22af54857ea
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-28 10:40:50 +0100

    potenza di 2

--- snack-3 copy/script.js
+++ snack-3 copy/script.js
@@ -1,24 +1,6 @@
-// Calcola la somma dei primi 10 numeri di un array.
+let power = 2;
 
-const myArray= [1 , 2, 32, 4, 5, 6, 17, 8, 9, 13, 11, 12, 13,];
-
-let sum= 0;
-
-let max= 0;
-
-for(let i = 0 ; i <10; i++){
-  sum += myArray[i];
-  if(myArray[i]>max){
-    max = myArray[i];
-  }
-
+while (power < 1000){
+  power *= 2
+  console.log(power)
 }
-
-
-console.log(sum);
-
-let myMedia= sum / 10;
-
-console.log(myMedia);
-
-console.log(max);
---
+++ snack-4/script.js
@@ -0,0 +1,7 @@
+// Calcola la somma dei primi 10 numeri di un array.
+let power = 2;
+
+while(power<1000){
+  power*= 2;
+  console.log(power);
+}
