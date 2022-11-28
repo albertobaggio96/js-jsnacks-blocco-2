4da488b1d382132a3dda798728306110824dc95e
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-28 12:45:08 +0100

    add ;

--- snack-6/script.js
+++ snack-6/script.js
@@ -4,12 +4,11 @@ const numberArray= [2, 13, 15, 22, 45, 55, 75, 3, 1];
 
 let sum= 0;
 
-
-
 for(i=0; i<numberArray.length; i++){
   if(i % 2 === 1){
-    sum += numberArray[i]
-    console.log(sum)
+    sum += numberArray[i];
   }
 }
+
+console.log(sum);
 
